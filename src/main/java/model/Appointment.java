2e57e835d64fd763c7d12d2a373e8de0a25fc922
Tungtahe180819 <tungtahe180819@fpt.edu.vn>
2025-06-11 04:02:47 +0700

    List Appointment

--- src/main/java/model/Appointment.java
+++ src/main/java/model/Appointment.java
@@ -6,7 +6,11 @@ public class Appointment {
     private int appointment_id;
     private int doctor_id;
     private int patient_id;
+<<<<<<< HEAD
     private Timestamp appointment_datetime;
+=======
+    private Timestamp appointment_date_time;
+>>>>>>> fce8356 (List Appointment)
     private int receptionist_id;
     private String shift;
     private String status;
@@ -15,12 +19,21 @@ public class Appointment {
     public Appointment() {
     }
 
+<<<<<<< HEAD
     public Appointment(int appointment_id, int doctor_id, int patient_id, Timestamp appointment_datetime, int receptionist_id, String shift,
                        String status, String note) {
         this.appointment_id = appointment_id;
         this.doctor_id = doctor_id;
         this.patient_id = patient_id;
         this.appointment_datetime = appointment_datetime;
+=======
+    public Appointment(int appointment_id, int doctor_id, int patient_id, Timestamp appointment_date_time,
+                       int receptionist_id, String shift, String status, String note) {
+        this.appointment_id = appointment_id;
+        this.doctor_id = doctor_id;
+        this.patient_id = patient_id;
+        this.appointment_date_time = appointment_date_time;
+>>>>>>> fce8356 (List Appointment)
         this.receptionist_id = receptionist_id;
         this.shift = shift;
         this.status = status;
@@ -51,12 +64,21 @@ public class Appointment {
         this.patient_id = patient_id;
     }
 
+<<<<<<< HEAD
     public Timestamp getAppointment_datetime() {
         return appointment_datetime;
     }
 
     public void setAppointment_datetime(Timestamp appointment_datetime) {
         this.appointment_datetime = appointment_datetime;
+=======
+    public Timestamp getAppointment_date_time() {
+        return appointment_date_time;
+    }
+
+    public void setAppointment_date_time(Timestamp appointment_date_time) {
+        this.appointment_date_time = appointment_date_time;
+>>>>>>> fce8356 (List Appointment)
     }
 
     public int getReceptionist_id() {
