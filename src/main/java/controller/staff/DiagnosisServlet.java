88ff6ec90699bdc82c434044b5ae485f9ec7e0b6
NguyenHuuKhai23 <khainhhe180234@fpt.edu.vn>
2025-06-13 21:05:25 +0700

    Khai update date diagnosisservlet

---
+++ src/main/java/controller/staff/DiagnosisServlet.java
@@ -0,0 +1,26 @@
+package controller.staff;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/staff/diagnosis")
+public class DiagnosisServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        HttpSession session = req.getSession(false);
+        if (session == null || session.getAttribute("user") == null) {
+            resp.sendRedirect(req.getContextPath() + "/home");
+            return;
+        }
+
+        req.getRequestDispatcher("/view/staff/Diagnosis.html").forward(req, resp);
+    }
+}
